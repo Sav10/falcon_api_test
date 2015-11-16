10330de4dc2ce885a03d049004f67260f1aa817c
Sav10 <sav.derivet@gmail.com>
2015-11-16 19:36:54 +0100

    e10

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ class Resource(object):
         abc = "aaa"
         try :
             conn = sqlite3.connect('iptable.db')
-            #conn.execute("INSERT INTO IPTEST (IP) VALUES (?)", [ip])
-            #conn.commit()
+            conn.execute("INSERT INTO IPTEST (IP) VALUES (?)", [ip])
+            conn.commit()
             cur = conn.cursor()
             cur.execute("SELECT *, COUNT(*) FROM IPTEST")
-            #row_db = cur.fetchone()
-            abc = str(cur.fetchone()[0])
+            row_db = cur.fetchone()
+            abc = str(row_db[0])
             conn.close()
         except Exception as e:
             abc = e
