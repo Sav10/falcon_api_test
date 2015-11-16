f8058fbbf4b7c324be0dcbb2cfb0f068c09f1323
Sav10 <sav.derivet@gmail.com>
2015-11-16 17:26:24 +0100

    for virtual env

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,2 +1,6 @@
 import sys, os
+import sys, os
+INTERP = "/var/www/myappfalcon1/code/flaskproject27/local/bin/python"
+#INTERP is present twice so that the new python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 from app import api as application
