4e608c82e6213e733c229a15f657d7d21bb78ad3
Sav10 <sav.derivet@gmail.com>
2015-11-16 13:33:33 +0100

    home page

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ class Resource(object):
         resp.body = ip + ' - ' + id_
         resp.status = falcon.HTTP_200
 
+class Homepage(object):
+
+    def on_get(self, req, resp):
+        ##resp.data = msgpack.packb({'message': 'Hello world!'})
+        ##resp.content_type = 'application/msgpack'
+        resp.body = "this is the api's Home page"
+        resp.status = falcon.HTTP_200
+
 api = application = falcon.API()
+
+api.add_route('/', Homepage())
 
 api.add_route('/req1', Resource())
