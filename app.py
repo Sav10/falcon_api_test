61ab2a584968c6956c43e4cfc2311f039bb17650
Sav10 <sav.derivet@gmail.com>
2015-11-17 15:39:02 +0100

    41

--- app.py
+++ app.py
@@ -6,19 +6,17 @@ class Resource(object):
 
     def on_get(self, req, resp):
         ip = req.env['REMOTE_ADDR']
-        abc = "aaa"
         ip2 = str(ip)
-        ip2 = "123456"
-        with sqlite3.connect('/var/db_dtp/iptable.db') as conn:
+        with sqlite3.connect('iptable.db') as conn:
             cur = conn.cursor()
             cur.execute("INSERT INTO IPTEST VALUES (NULL, ?)", (ip2,))
             cur.execute("SELECT *, COUNT(*) FROM IPTEST")
         row_db = cur.fetchone()
-        abc = str(row_db[0])
+        num_rec = str(row_db[0])
         conn.commit()
         conn.close()
         id_ = req.params['id']
-        resp.body = ip + ' - ' + id_ + ' - ' + abc
+        resp.body = ip + ' - ' + id_ + ' - ' + num_rec
         resp.status = falcon.HTTP_200
 
 class Homepage(object):
